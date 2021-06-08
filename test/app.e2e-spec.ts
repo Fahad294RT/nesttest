0be9b235d05401c6cd1c2dcf0363e6b024b3ed9f
Fahad Khan <fahad@rebeltechnology.io>
2021-06-08 18:07:22 +0500

    e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,10 +3,19 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
+const photo= {
+  "name": "Some photo e2e",
+  "description": "Some description",
+  "filename": "some file",
+  "views": 0,
+  "isPublished": false,
+  "user": 4
+}
+
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -21,4 +30,27 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+
+  describe('Photo e2e', ()=>{
+
+    it('/ (GET)', async () => {
+      const data = await request(app.getHttpServer())
+        .post('/photo')
+        .send(photo)
+        .expect(201);
+
+      expect(data.body).toEqual({
+        ...photo,
+        id: expect.any(Number),
+        status: "draft",
+        updated: expect.any(String),
+        created: expect.any(String),
+      });
+
+      console.log(`Item was added at ID ${data.body.id}`)
+      return request(app.getHttpServer())
+        .delete(`/photo/${data.body.id}`)
+        .expect(200)
+    });
+  })
 });
